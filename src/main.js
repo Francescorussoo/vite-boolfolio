534683bfad8e1af777de908090ce90466f0c3d6c
Francescorussoo <164784843+Francescorussoo@users.noreply.github.com>
2024-11-10 15:06:05 +0100

    aggiornato main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import Home from './views/Home.vue';
+import Projects from './views/Projects.vue';
+import About from './views/About.vue';
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', name: 'home', component: Home },
+  { path: '/projects', name: 'projects', component: Projects },
+  { path: '/about', name: 'about', component: About },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
